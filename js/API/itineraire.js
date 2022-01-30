113c83e9337f42d5a0263fe5263710ff1bc93786
Thomas <thomasdulompont.1@gmail.com>
2022-01-30 23:30:10 +0100

    Ajout des fichiers itineraires

--- js/API/itineraire.js
+++ js/API/itineraire.js
@@ -1,27 +1,50 @@
-const containerEtapes = document.querySelector(".container-etapes");
-const url = "http://localhost:1337";
-let allEtapes = [];
+const info = document.querySelector(".grille-etapes");
+const url = "http://195.14.105.18:1337"
+
+let dataResult = [];
+
 init();
 
 function init() {
-    getEtapes();
+  getEtapes();
+  renderData();
+}
+
+// r.data[n].attributes.city_start
+
+function renderData(data) {
+  r = Object(data)
+  for (let n in r.data){
+    console.log(data);
+    const content = `
+      <div class="pane-etapes">
+        <div class="image">
+            <img src="${url}${r.data[n].attributes.image.data.attributes.url}" alt="Image Velo">
+            <p class="kilometres">${r.data[n].attributes.km} Km</p>
+        </div>
+        <div class="section">
+            <ul class="banniere">
+                <li class="text">Nature, Patrimoine</li>
+                <li class="like"><a href="#"><i class="far fa-heart"></i></a></li>
+            </ul>
+            <div class="difficulty">
+                <p>${r.data[n].attributes.difficulty}</p>
+            </div>
+            <h2>${r.data[n].attributes.city_start} / ${r.data[n].attributes.city_end}</h2>
+            <p class="description">${r.data[n].attributes.description}</p>
+        </div>
+      </div>
+      `;
+
+      info.innerHTML += content;
+  }
 }
 
 function getEtapes() {
-    fetch(`${url}/api/etapes`)
-        .then((data) => data.json())
-        .then((result) => {
-            allEtapes = result;
-            console.log("allEtapes", allEtapes);
-            renderEtapes(allEtapes);
-        })
-}
-
-function renderEtapes(etapes) {
-    let list = [];
-    etapes.forEach((e) => {
-        const item = `<h1>${e.depart}</h1>`;
-        list = [...list, item];
-    })
-    console.log(list);
+  fetch(`${url}/api/etapes?populate=*`)
+    .then((data) => data.json())
+    .then((result) => {
+      dataResult = result;
+      renderData(dataResult);
+    });
 }
